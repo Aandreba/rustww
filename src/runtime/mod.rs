5f75eed27d5ceb335ec91b2267f0ec8c9bfb7866
Aandreba <aandrebafreelancer@gmail.com>
2022-12-20 15:49:43 +0100

    various

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,38 +1,23 @@
-use std::{sync::Arc, task::Waker};
+use std::{sync::Arc, pin::Pin, task::{Context, Poll}};
 use crossbeam::queue::SegQueue;
 use futures::Future;
-use utils_atomics::FillQueue;
-use crate::sync::one_shot;
-use self::task::Task;
-
 mod web;
 
-struct Inner {
-    queue: SegQueue<Task>
+pub(super) type DynFuture<T> = Pin<Box<dyn 'static + Send + Future<Output = T>>>;
+
+pub(super) struct Inner {
+    queue: SegQueue<Box<dyn FnOnce()>>
 }
 
 /// Handle to work with a runtime instance
 #[derive(Clone)]
 pub struct Handle {
-    inner: Arc<Inner>
+    pub(super) inner: Arc<Inner>
 }
 
 impl Handle {
     #[inline]
     pub fn schedule<T> (&self, task: Task) {
         self.inner.queue.push(task)
-    }
-}
-
-pub struct Scheduler {
-    handle: Handle,
-}
-
-impl Scheduler {
-    #[inline]
-    pub fn run (self) {
-        loop {
-            
-        }
     }
 }
--- src/runtime/web.rs
+++ src/runtime/web.rs
@@ -1,9 +1,8 @@
-use std::{task::{Poll, RawWaker, RawWakerVTable, Waker, Context}, sync::Arc, marker::PhantomPinned, pin::Pin, cell::Cell};
+use std::{task::{Poll, RawWaker, RawWakerVTable, Context, Waker}, sync::{Arc, atomic::AtomicUsize, Mutex, TryLockError, MutexGuard}, ops::DerefMut, pin::Pin};
 use futures::{Future, FutureExt};
-use pin_project_lite::pin_project;
-use utils_atomics::TakeCell;
 use wasm_bindgen::{prelude::*, JsCast};
-use crate::sync::{one_shot, ShotReceiver, ShotSender, DropHandle, drop_local};
+use crate::{sync::{one_shot, drop_local}, runtime::DynFuture};
+use super::{Handle, Task};
 
 #[wasm_bindgen]
 extern "C" {
@@ -15,36 +14,44 @@ extern "C" {
 
 impl Scheduler {
     #[inline]
-    pub fn schedule_closure<T: 'static, F: 'static + FnOnce() -> T> (&self, f: F) -> ClosureHandle<T> {
-        let (send, recv) = one_shot();
-        let closure = Closure::once(move || send.send(f()));
+    pub fn spawn_boxed (fut: DynFuture<()>) {        
+        todo!()
+    }
+}
 
-        let fun = closure.as_ref();
-        debug_assert!(fun.is_instance_of::<js_sys::Function>());
-        let _ = self.post_task(fun.unchecked_ref());
+fn create_raw_waker<T: 'static + Send> (handle: Handle, fut: DynFuture<()>) -> RawWaker {
+    type Inner = (Handle, DynFuture<()>);
+    
+    static VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+
+    unsafe fn clone (ptr: *const ()) -> RawWaker {
+        Arc::increment_strong_count(ptr as *const Inner);
+        return RawWaker::new(ptr, &VTABLE)
+    }
+
+    unsafe fn wake (ptr: *const ()) {
+        let raw = RawWaker::new(ptr, &VTABLE);
+        let waker = Waker::from_raw(raw);
+        let mut cx = Context::from_waker(&waker);
         
-        return ClosureHandle {
-            cb: drop_local(closure),
-            recv
-        }
+        let inner = Arc::from_raw(ptr as *const Inner);
+        inner.0.inner.queue.push(Box::new(|| {
+            let raw = RawWaker::new(ptr, &VTABLE);
+            let waker = Waker::from_raw(raw);
+            let mut cx = Context::from_waker(&waker);
+            inner.1
+        }));<
     }
-}
 
-/// Handle to wait for a scheduled closure
-struct ClosureHandle<T> {
-    cb: DropHandle,
-    recv: ShotReceiver<T>
-}
-
-impl Future for ClosureHandle<T> {
-    type Output = T;
-
-    #[inline]
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        return match self.recv.poll_unpin(cx) {
-            Poll::Ready(Some(x)) => Poll::Ready(x),
-            Poll::Ready(None) => unreachable!(),
-            Poll::Pending => Poll::Pending
-        }
+    unsafe fn wake_by_ref (ptr: *const ()) {
+        
     }
+
+    unsafe fn drop (ptr: *const ()) {
+        Arc::decrement_strong_count(ptr as *const Inner);
+    }
+
+    let data = Arc::new((handle, fut));
+
+    todo!()
 }
